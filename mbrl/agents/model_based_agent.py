Fix normalization bug and add state to all learnable components; remove naming components; create utility file for operations on Gaussians; remove discounting in rollout utility function

mbrl/agents/model_based_agent.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from functools import partial
 from typing import Any, Callable, Dict
 
 from gym.envs.mujoco import MujocoEnv
@@ -56,15 +57,16 @@ class DeepModelBasedAgent(ABC):
         )
 
         params_per_member = [{} for _ in range(self._ensemble_size)]
+        state_per_member = [{} for _ in range(self._ensemble_size)]
         for idx in range(self._ensemble_size):
             self._rng_key, subkey = jax.random.split(self._rng_key)
-            params_per_member[idx] = self._dynamics_model.init(params_per_member[idx], subkey)
+            params_per_member[idx], state_per_member[idx] = \
+                self._dynamics_model.init(params_per_member[idx], state_per_member[idx], subkey)
         self._dynamics_params = jax.tree_map(lambda *a: jnp.stack(a), *params_per_member)
-
-        self._model_eval_op = self._create_model_eval_op()
+        self._dynamics_state = jax.tree_map(lambda *a: jnp.stack(a), *state_per_member)
 
         self._dynamics_optimizer_state = self._dynamics_optimizer.init(self._dynamics_params)
-        self._model_update_op = self._create_model_update_op()
+        self._model_update_op = jax.jit(self._update_model)
 
     def reset(self) -> None:
         """Resets the agent after an environment rollout. Does nothing by default. """
@@ -96,11 +98,14 @@ class DeepModelBasedAgent(ABC):
     def train(self) -> None:
         """Trains the internal dynamics model of this agent with all the provided interaction data so far.
         """
-        self._dynamics_model.fit_normalizer(
-            self._dynamics_dataset["observation"],
-            self._dynamics_dataset["action"],
-            self._dynamics_dataset["next_observation"]
-        )
+        self._dynamics_params, self._dynamics_state = \
+            jax.vmap(self._dynamics_model.fit_normalizer, (0, 0, None, None, None))(
+                self._dynamics_params,
+                self._dynamics_state,
+                self._dynamics_dataset["observation"],
+                self._dynamics_dataset["action"],
+                self._dynamics_dataset["next_observation"]
+            )
 
         self._rng_key, subkey = jax.random.split(self._rng_key)
         bootstrapped_dataset = self._dynamics_dataset.bootstrap(self._ensemble_size, subkey)
@@ -116,8 +121,8 @@ class DeepModelBasedAgent(ABC):
                     self._rng_key, subkey = jax.random.split(self._rng_key)
                     epoch_iterator = bootstrapped_dataset.epoch(self._model_train_batch_size, subkey)
 
-            self._dynamics_params, self._dynamics_optimizer_state = self._model_update_op(
-                self._dynamics_params, self._dynamics_optimizer_state, batches_per_member
+            self._dynamics_params, self._dynamics_state, self._dynamics_optimizer_state = self._model_update_op(
+                self._dynamics_params, self._dynamics_state, self._dynamics_optimizer_state, batches_per_member
             )
 
     @abstractmethod
@@ -148,64 +153,62 @@ class DeepModelBasedAgent(ABC):
         self._rng_key, subkey = jax.random.split(self._rng_key)
         iterator = self._dynamics_dataset.epoch(self._n_model_eval_points, subkey, full_batch_required=False)
         return {
-            "Dynamics model log-likelihood": self._model_eval_op(self._dynamics_params, next(iterator))
+            "Dynamics model log-likelihood": self._evaluate_model_log_likelihood(
+                self._dynamics_params, self._dynamics_state, next(iterator)
+            )
         }
 
-    def _create_model_eval_op(self):
-        @jax.jit
-        def ensemble_eval(dynamics_params, eval_batch):
-            def batch_mean_loss(single_net_params, batch):
-                return jnp.mean(jax.vmap(self._dynamics_model.prediction_loss, (None, 0, 0, 0))(
-                    single_net_params, batch["observation"], batch["action"], batch["next_observation"]
-                ))
-            return jnp.mean(jax.vmap(batch_mean_loss, (0, None))(dynamics_params, eval_batch))
-
-        return ensemble_eval
-
-    def _create_model_update_op(self):
-        @jax.jit
-        def model_update(dynamics_params, dynamics_optimizer_state, all_member_batches):
-            def batch_mean_loss(single_net_params, batch):
-                return jnp.mean(jax.vmap(self._dynamics_model.prediction_loss, (None, 0, 0, 0))(
-                    single_net_params, batch["observation"], batch["action"], batch["next_observation"]
-                ))
-
-            def sum_ensemble_losses(ensemble_params, ensemble_batches):
-                return jnp.sum(jax.vmap(batch_mean_loss)(ensemble_params, ensemble_batches))
-
-            ensemble_grads = jax.grad(sum_ensemble_losses)(dynamics_params, all_member_batches)
-            updates, dynamics_optimizer_state = self._dynamics_optimizer.update(
-                ensemble_grads, dynamics_optimizer_state, dynamics_params
+    def _evaluate_model_log_likelihood(self, dynamics_params, dynamics_state, evaluation_batch):
+        def batch_mean_log_likelihood(single_net_params, single_net_state, batch):
+            single_net_evaluator = partial(self._dynamics_model.prediction_loss, single_net_params, single_net_state)
+            log_likelihoods = -jax.vmap(single_net_evaluator)(
+                batch["observation"], batch["action"], batch["next_observation"]
             )
-            return optax.apply_updates(dynamics_params, updates), dynamics_optimizer_state
+            return jnp.mean(log_likelihoods)
 
-        return model_update
+        ensemble_mean_likelihoods = jax.vmap(batch_mean_log_likelihood, (0, 0, None))(
+            dynamics_params, dynamics_state, evaluation_batch
+        )
+        return jnp.mean(ensemble_mean_likelihoods)
+
+    def _update_model(self, dynamics_params, dynamics_state, dynamics_optimizer_state, all_member_batches):
+        def batch_mean_loss(single_net_params, single_net_state, batch):
+            single_net_evaluator = partial(self._dynamics_model.prediction_loss, single_net_params, single_net_state)
+            losses = jax.vmap(single_net_evaluator)(
+                batch["observation"], batch["action"], batch["next_observation"]
+            )
+            return jnp.mean(losses)
+
+        def sum_ensemble_losses(ensemble_params, ensemble_state, ensemble_batches):
+            return jnp.sum(jax.vmap(batch_mean_loss)(ensemble_params, ensemble_state, ensemble_batches))
+
+        ensemble_grads = jax.grad(sum_ensemble_losses)(dynamics_params, dynamics_state, all_member_batches)
+        updates, dynamics_optimizer_state = self._dynamics_optimizer.update(
+            ensemble_grads, dynamics_optimizer_state, dynamics_params
+        )
+        return optax.apply_updates(dynamics_params, updates), dynamics_state, dynamics_optimizer_state
 
     def _create_rollout_evaluator(
         self,
         rollout_policy: Callable[[Any, jnp.ndarray, int, jax.random.KeyArray], jnp.ndarray],
         rollout_horizon: int,
-        fn_to_accumulate: Callable[[int, jnp.ndarray, jnp.ndarray, jnp.ndarray, Dict, Dict, jax.random.KeyArray],
-                                   jnp.ndarray],
-        accumulator_init: Any = 0.,
-        discount_factor: float = 1.0
+        fn_to_accumulate: Callable[[Dict, Dict, Dict, Dict, jax.random.KeyArray],
+                                   Any],
+        accumulator_init: Any
     ):
         """Helper method that creates evaluators using the model to roll out policies.
         Created so that the rollout_policy parameters are JAX vmap-able.
 
         Args:
-            rollout_policy: A policy mapping of the form (params, obs, i, rng_key) -> action.
+            rollout_policy: A policy mapping of the form (rollout_policy_params, obs, timestep, rng_key) -> action.
                 See examples below.
             rollout_horizon: Rollout horizon
             fn_to_accumulate: A scalar- or pytree-valued function on
-                (observation, action, next_observation, rollout_policy_params, dynamics_params, rng_key)
-                that will be accumulated over the rollout for trajectory evaluation.
-                For example, setting this to self.reward_fn will return an evaluator that computes the rollout return.
+                (rollout_policy_params, dynamics_params, dynamics_state, timestep_information, rng_key)
+                that will be summed over the rollout for trajectory evaluation.
                 rng_key is provided to allow for random functions (e.g. entropy bonus estimated from samples)
-            accumulator_init: Initial value for the accumulator.
-                Defaults to 0 for backwards compatibility, which is valid only for scalar-valued fn_to_accumulate.
-            discount_factor: Discount factor used for computing returns.
-                Defaults to 1.0 (i.e. no discount).
+            accumulator_init: Initial value for the accumulator, must be of the same type as the output of
+                fn_to_accumulate.
 
         Returns:
             Policy evaluator which, given (rollout policy params, dynamics parameters, start_obs, JAX RNG key),
@@ -227,7 +230,7 @@ class DeepModelBasedAgent(ABC):
         ...     self._wrap_basic_reward(self._reward_fn)
         ... )
         """
-        def rollout_and_evaluate(rollout_policy_params, dynamics_params, start_obs, rng_key):
+        def rollout_and_evaluate(rollout_policy_params, dynamics_params, dynamics_state, start_obs, rng_key):
             # Assign an ensemble member to each particle at random
             rng_key, subkey = jax.random.split(rng_key)
             member = jax.random.randint(
@@ -236,35 +239,43 @@ class DeepModelBasedAgent(ABC):
             member_params = jax.tree_map(
                 lambda all_params: all_params[member], dynamics_params
             )
+            member_state = jax.tree_map(
+                lambda all_states: all_states[member], dynamics_state
+            )
 
-            def simulate_single_timestep(prev_timestep_info, h):
-                cur_obs, cur_return, r_key = prev_timestep_info
+            def simulate_single_timestep(prev_timestep_carry, h):
+                cur_obs, cur_return, r_key = prev_timestep_carry
 
                 r_key, s_key = jax.random.split(r_key)
                 action = rollout_policy(rollout_policy_params, cur_obs, h, s_key)
 
                 r_key, s_key = jax.random.split(r_key)
-                next_obs = self._dynamics_model.predict(member_params, cur_obs, action, s_key)
+                next_obs = self._dynamics_model.predict(member_params, member_state, cur_obs, action, s_key)
+
+                timestep_info = {
+                    "timestep": h,
+                    "observation": cur_obs,
+                    "action": action,
+                    "next_observation": next_obs
+                }
 
                 r_key, s_key = jax.random.split(r_key)
                 cur_return = jax.tree_map(
-                    lambda x, y: x + (discount_factor ** h) * y,
+                    lambda x, y: x + y,
                     cur_return,
-                    fn_to_accumulate(
-                        h, cur_obs, action, next_obs, rollout_policy_params, member_params, s_key
-                    )
+                    fn_to_accumulate(rollout_policy_params, member_params, member_state, timestep_info, s_key)
                 )
 
                 return (next_obs, cur_return, r_key), cur_obs
 
-            (final_obs, rollout_return, _), obs_seq = jax.lax.scan(
+            (final_obs, accumulation, _), obs_seq = jax.lax.scan(
                 simulate_single_timestep,
                 (start_obs, accumulator_init, rng_key),
                 jnp.arange(rollout_horizon, dtype=int)
             )
             return {
                 "final_observation": final_obs,
-                "rollout_return": rollout_return,
+                "accumulation": accumulation,
                 "observation_sequence": jnp.append(obs_seq, final_obs[None], axis=0)
             }
 
@@ -275,5 +286,5 @@ class DeepModelBasedAgent(ABC):
         """Convenience function which wraps a basic reward function in another function
         that is compatible with create_rollout_evaluator.
         """
-        return lambda _, observation, action, next_observation, __, ___, ____: \
-            reward_fn(observation, action, next_observation)
+        return lambda _, __, ___, timestep_info, ____: \
+            reward_fn(timestep_info["observation"], timestep_info["action"], timestep_info["next_observation"])

mbrl/agents/model_based_policy_agent.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Callable
 
 from gym.envs.mujoco import MujocoEnv
@@ -75,7 +76,7 @@ class ModelBasedPolicyAgent(DeepModelBasedAgent):
         self._policy_dataset = Dataset(observation=env.observation_space.shape)
 
         self._rng_key, subkey = jax.random.split(self._rng_key)
-        self._policy_params = self._policy.init({}, subkey)
+        self._policy_params, self._policy_state = self._policy.init({}, {}, subkey)
 
         self._policy_optimizer_state = self._policy_optimizer.init(self._policy_params)
         self._policy_update_op = self._create_policy_update_op()
@@ -114,8 +115,12 @@ class ModelBasedPolicyAgent(DeepModelBasedAgent):
                     epoch_iterator = self._policy_dataset.epoch(self._policy_train_batch_size, subkey)
 
             self._rng_key, subkey = jax.random.split(self._rng_key)
-            self._policy_params, self._policy_optimizer_state = self._policy_update_op(
-                self._policy_params, self._dynamics_params, self._policy_optimizer_state,
+            self._policy_params, self._policy_state, self._policy_optimizer_state = self._policy_update_op(
+                self._policy_params,
+                self._policy_state,
+                self._dynamics_params,
+                self._dynamics_state,
+                self._policy_optimizer_state,
                 batch_obs,
                 subkey
             )
@@ -133,31 +138,63 @@ class ModelBasedPolicyAgent(DeepModelBasedAgent):
             Action chosen by the agent
         """
         self._rng_key, subkey = jax.random.split(self._rng_key)
-        return onp.array(self._policy.act(self._policy_params, obs, subkey))
+        return onp.array(self._policy.act(self._policy_params, self._policy_state, obs, subkey))
 
     def _create_policy_update_op(self):
+        def rollout_policy(policy_params_and_state, obs, _timestep, rng_key):
+            policy_params = policy_params_and_state["params"]
+            policy_state = policy_params_and_state["state"]
+            return self._policy.act(policy_params, policy_state, obs, rng_key)
+
         rollout_and_evaluate = self._create_rollout_evaluator(
-            rollout_policy=lambda policy_params, obs, _i, rng_key: self._policy.act(policy_params, obs, rng_key),
+            rollout_policy=rollout_policy,
             rollout_horizon=self._plan_horizon,
-            fn_to_accumulate=self._wrap_basic_reward(self._reward_fn)
+            fn_to_accumulate=self._wrap_basic_reward(self._reward_fn),
+            accumulator_init=0.
         )
 
-        def multiple_particle_evaluator(policy_params, dynamics_params, start_obs, rng_key):
-            particle_returns = jax.vmap(rollout_and_evaluate, (None, None, None, 0))(
-                policy_params, dynamics_params, start_obs, jax.random.split(rng_key, num=self._n_particles)
-            )["rollout_return"]
+        def multiple_particle_evaluator(
+            policy_params,
+            policy_state,
+            dynamics_params,
+            dynamics_state,
+            start_obs,
+            rng_key
+        ):
+            policy_params_and_state = {
+                "params": policy_params,
+                "state": policy_state
+            }
+            single_particle_evaluator = partial(
+                rollout_and_evaluate, policy_params_and_state, dynamics_params, dynamics_state, start_obs
+            )
+            particle_returns = \
+                jax.vmap(single_particle_evaluator)(jax.random.split(rng_key, num=self._n_particles))["accumulation"]
             return jnp.mean(particle_returns)
 
         @jax.jit
-        def perform_policy_update(policy_params, dynamics_params, policy_optimizer_state, batch_start, rng_key):
+        def perform_policy_update(
+            policy_params,
+            policy_state,
+            dynamics_params,
+            dynamics_state,
+            policy_optimizer_state,
+            batch_start,
+            rng_key
+        ):
             def mean_batch_cost_to_go(*args):
-                return -jnp.mean(jax.vmap(multiple_particle_evaluator, (None, None, 0, 0))(*args))
+                return -jnp.mean(jax.vmap(multiple_particle_evaluator, (None, None, None, None, 0, 0))(*args))
 
             batch_grad = jax.grad(mean_batch_cost_to_go)(
-                policy_params, dynamics_params, batch_start, jax.random.split(rng_key, num=batch_start.shape[0])
+                policy_params,
+                policy_state,
+                dynamics_params,
+                dynamics_state,
+                batch_start,
+                jax.random.split(rng_key, num=len(batch_start))
             )
             updates, policy_optimizer_state = \
                 self._policy_optimizer.update(batch_grad, policy_optimizer_state, policy_params)
-            return optax.apply_updates(policy_params, updates), policy_optimizer_state
+            return optax.apply_updates(policy_params, updates), policy_state, policy_optimizer_state
 
         return perform_policy_update

mbrl/agents/model_predictive_control_agent.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Callable
 
 from gym.envs.mujoco import MujocoEnv
@@ -100,7 +101,9 @@ class ModelPredictiveControlAgent(DeepModelBasedAgent):
             Action chosen by the agent
         """
         self._rng_key, subkey = jax.random.split(self._rng_key)
-        optimized_action_seq_dist = self._cem_op(self._dynamics_params, obs, self._proposal_dist, subkey)
+        optimized_action_seq_dist = self._cem_op(
+            self._dynamics_params, self._dynamics_state, obs, self._proposal_dist, subkey
+        )
 
         next_proposal_dist_mean = jnp.concatenate([
             optimized_action_seq_dist["mean"][1:],
@@ -118,17 +121,19 @@ class ModelPredictiveControlAgent(DeepModelBasedAgent):
         rollout_and_evaluate = self._create_rollout_evaluator(
             rollout_policy=lambda action_seq, _obs, i, _rng_key: action_seq[i],
             rollout_horizon=self._plan_horizon,
-            fn_to_accumulate=self._wrap_basic_reward(self._reward_fn)
+            fn_to_accumulate=self._wrap_basic_reward(self._reward_fn),
+            accumulator_init=0.
         )
 
-        def multiple_particle_evaluator(action_seq, dynamics_params, start_obs, rng_key):
-            particle_returns = jax.vmap(rollout_and_evaluate, (None, None, None, 0))(
-                action_seq, dynamics_params, start_obs, jax.random.split(rng_key, num=self._n_particles)
-            )["rollout_return"]
+        def multiple_particle_evaluator(action_seq, dynamics_params, dynamics_state, start_obs, rng_key):
+            single_particle_evaluator = \
+                partial(rollout_and_evaluate, action_seq, dynamics_params, dynamics_state, start_obs)
+            particle_returns = \
+                jax.vmap(single_particle_evaluator)(jax.random.split(rng_key, num=self._n_particles))["accumulation"]
             return jnp.mean(particle_returns)
 
         @jax.jit
-        def cross_entropy_method_update(dynamics_params, cur_obs, proposal_dist, rng_key):
+        def cross_entropy_method_update(dynamics_params, dynamics_state, cur_obs, proposal_dist, rng_key):
             rng_key, subkey = jax.random.split(rng_key)
             pre_bounding_candidates = proposal_dist["mean"][None] + proposal_dist["stddev"][None] * \
                 jax.random.normal(subkey, shape=[self._n_candidates] + list(proposal_dist["mean"].shape))
@@ -136,8 +141,12 @@ class ModelPredictiveControlAgent(DeepModelBasedAgent):
             action_seq_candidates = self._proposal_space_to_actions(pre_bounding_candidates)
 
             rng_key, subkey = jax.random.split(rng_key)
-            candidate_evals = jax.vmap(multiple_particle_evaluator, (0, None, None, 0))(
-                action_seq_candidates, dynamics_params, cur_obs, jax.random.split(subkey, num=self._n_candidates)
+            candidate_evals = jax.vmap(multiple_particle_evaluator, (0, None, None, None, 0))(
+                action_seq_candidates,
+                dynamics_params,
+                dynamics_state,
+                cur_obs,
+                jax.random.split(subkey, num=self._n_candidates)
             )
 
             sorting_idxs = jnp.argsort(candidate_evals)
@@ -149,12 +158,12 @@ class ModelPredictiveControlAgent(DeepModelBasedAgent):
 
             return updated_proposal_dist
 
-        def cross_entropy_method(dynamics_params, cur_obs, proposal_dist, rng_key):
+        def cross_entropy_method(dynamics_params, dynamics_state, cur_obs, proposal_dist, rng_key):
             n_iters = 0
             while n_iters < self._max_cem_iters and jnp.max(proposal_dist["stddev"]) > self._cem_epsilon:
                 rng_key, subkey = jax.random.split(rng_key)
                 proposal_dist = cross_entropy_method_update(
-                    dynamics_params, cur_obs, proposal_dist, subkey
+                    dynamics_params, dynamics_state, cur_obs, proposal_dist, subkey
                 )
                 n_iters += 1
             return proposal_dist

mbrl/misc/fully_connected_neural_net.py
@@ -9,7 +9,6 @@ from mbrl._src.utils import Array
 class FullyConnectedNeuralNet:
     def __init__(
         self,
-        name: str,
         input_dim: int,
         output_dim: int,
         hidden_dims: List[int],
@@ -19,7 +18,6 @@ class FullyConnectedNeuralNet:
         """Creates a fully-connected network.
 
         Args:
-            name: Network name. Used for referencing parameters, so must be unique.
             input_dim: Net input dimension.
             output_dim: Net output dimension.
             hidden_dims: List of hidden layer dimensions.
@@ -28,15 +26,14 @@ class FullyConnectedNeuralNet:
             output_activation: Activation to use for the final layer.
                 Defaults to the identity operation.
 
-        >>> net = FullyConnectedNeuralNet("relu_net", 5, 10, [100], jax.nn.relu)   # Creates a network R^5 -> R^10 with a single
-                                                                          #   hidden layer of 100 ReLU neurons.
+        >>> net = FullyConnectedNeuralNet(5, 10, [100], jax.nn.relu)   # Creates a network R^5 -> R^10 with a single
+                                                                       #   hidden layer of 100 ReLU neurons.
         """
         if not isinstance(hidden_activations, list):
             hidden_activations = len(hidden_dims) * [hidden_activations]
         if len(hidden_dims) != len(hidden_activations):
             raise ValueError("Number of hidden layers does not match number of hidden activations provided.")
 
-        self._name = name
         self._input_dim = input_dim
         self._intermediate_dims = hidden_dims + [output_dim]
         self._intermediate_activations = hidden_activations + [output_activation]
@@ -44,53 +41,51 @@ class FullyConnectedNeuralNet:
     def init(
         self,
         params: Dict,
+        state: Dict,
         rng_key: jax.random.KeyArray
-    ) -> Dict:
-        """Places randomly initialized net parameters within the given dictionary, which is returned.
+    ) -> (Dict, Dict):
+        """Places randomly initialized net parameters and state within the given dictionaries, which are returned.
 
         Args:
-            params: Dictionary of parameters. Sets params[<network name>] to initialized parameters.
+            params: Dictionary where the network will place its own parameters.
+            state: Dictionary where the network will place its internal state.
             rng_key: JAX RNG key, which should not be reused outside this function.
 
         Returns:
             params
         """
-        net_params = {}
         cur_dim = self._input_dim
         for depth, out_dim in enumerate(self._intermediate_dims):
-            layer_params = net_params["transform{}".format(depth)] = {}
+            layer_params = params["transform{}".format(depth)] = {}
             rng_key, subkey = jax.random.split(rng_key)
-            layer_params["weights"] = \
-                jnp.sqrt(2 / cur_dim) * jax.random.normal(subkey, shape=[out_dim, cur_dim])   # Kaiming init
+            layer_params["weights"] = jnp.sqrt(2 / cur_dim) * jax.random.normal(subkey, shape=[out_dim, cur_dim])
             layer_params["biases"] = jnp.zeros(out_dim)
             cur_dim = out_dim
-
-        params[self._name] = net_params
-        return params
+        return params, state
 
     def forward(
         self,
         params: Dict,
+        _state: Dict,
         query: Array
     ) -> Any:
         """Returns the output of the network on a single query point, using the given parameter dictionary.
 
         Args:
-            params: Dictionary of parameters.
+            params: Dictionary of network parameters.
+            _state: Dictionary containing network state.
             query: Point on which network will be queried. query.shape = [input_dim].
 
         Returns:
             Network output evaluated at query. return.shape = [output_dim]
         """
         try:
-            net_params = params[self._name]
+            hidden = query
+            for depth, activation in enumerate(self._intermediate_activations):
+                layer_params = params["transform{}".format(depth)]
+                pre_activation = jnp.dot(layer_params["weights"], hidden) + layer_params["biases"]
+                hidden = pre_activation if activation is None else activation(pre_activation)
+            return hidden
         except KeyError:
-            raise RuntimeError("Cannot find network parameters in provided parameter dictionary. "
-                               "Have you called init()?")
+            raise RuntimeError("Missing parameters in provided parameter dictionary. Did you call init()?")
 
-        hidden = query
-        for depth, activation in enumerate(self._intermediate_activations):
-            layer_params = net_params["transform{}".format(depth)]
-            pre_activation = jnp.dot(layer_params["weights"], hidden) + layer_params["biases"]
-            hidden = pre_activation if activation is None else activation(pre_activation)
-        return hidden

mbrl/misc/neural_net_dynamics_model.py
@@ -4,13 +4,13 @@ import jax
 import jax.numpy as jnp
 
 from mbrl.misc.fully_connected_neural_net import FullyConnectedNeuralNet
-from mbrl._src.utils import Array, create_gaussianizer, gaussian_log_prob, normalize, reparameterized_gaussian_sampler
+from mbrl._src.utils import Array, normalize
+from mbrl._src.gaussian_utils import create_bounded_gaussianizer, gaussian_log_prob, reparameterized_gaussian_sampler
 
 
 class NeuralNetDynamicsModel:
     def __init__(
         self,
-        name: str,
         dummy_obs: Array,
         dummy_action: Array,
         hidden_dims: List[int],
@@ -27,7 +27,6 @@ class NeuralNetDynamicsModel:
         """Creates a dynamics model which internally uses a neural network with a deterministic output.
 
         Args:
-            name: Model name. Used for referencing parameters, so must be unique.
             dummy_obs: Single dummy observation used to infer shapes.
             dummy_action: Single dummy action used to infer shapes.
             hidden_dims: List of hidden dimensions for internal network.
@@ -45,7 +44,6 @@ class NeuralNetDynamicsModel:
             normalize_inputs: Indicates if input normalization will be used.
             normalize_outputs: Indicates if output normalization will be used.
         """
-        self._name = name
         self._obs_preproc = obs_preproc
         self._targ_comp = targ_comp
         self._next_obs_comp = next_obs_comp
@@ -53,28 +51,16 @@ class NeuralNetDynamicsModel:
         self._is_normalizing_inputs = normalize_inputs
         self._is_normalizing_outputs = normalize_outputs
 
-        preproc_obs_dim = self._obs_preproc(dummy_obs).shape[0]
-        action_dim = dummy_action.shape[0]
-        pred_dim = self._targ_comp(dummy_obs, dummy_obs).shape[0]
-
-        self._normalizer_params = {
-            name: {
-                "center": jnp.zeros(shape=[dim]),
-                "scale": jnp.ones(shape=[dim])
-            } for (name, dim) in [("input", preproc_obs_dim + action_dim), ("output", pred_dim)]
-        }
+        self._preproc_obs_dim = self._obs_preproc(dummy_obs).shape[0]
+        self._action_dim = dummy_action.shape[0]
+        self._pred_dim = self._targ_comp(dummy_obs, dummy_obs).shape[0]
 
         self._internal_net = FullyConnectedNeuralNet(
-            name="{}_internal".format(self._name),
-            input_dim=preproc_obs_dim + action_dim,
-            output_dim=2*pred_dim if self._is_probabilistic else pred_dim,
+            input_dim=self._preproc_obs_dim + self._action_dim,
+            output_dim=2*self._pred_dim if self._is_probabilistic else self._pred_dim,
             hidden_dims=hidden_dims,
             hidden_activations=hidden_activations,
-            output_activation=create_gaussianizer(
-                stddev_nonlinearity=jax.nn.softplus,
-                min_stddev=min_stddev,
-                max_stddev=max_stddev
-            ) if self._is_probabilistic else None
+            output_activation=create_bounded_gaussianizer(min_stddev, max_stddev) if self._is_probabilistic else None
         )
 
     @property
@@ -85,26 +71,51 @@ class NeuralNetDynamicsModel:
     def init(
         self,
         params: Dict,
+        state: Dict,
         rng_key: jax.random.KeyArray
-    ) -> Dict:
-        """Places randomly initialized net parameters within the given dictionary, which is returned.
+    ) -> (Dict, Dict):
+        """Places randomly initialized net parameters and state within the given dictionary, which is returned.
 
         Args:
-            params: Dictionary of parameters where initialization will be placed.
+            params: Dictionary where initialized model parameters will be placed.
+            state: Dictionary where model state will be placed.
             rng_key: JAX RNG key, which should not be reused outside this function.
 
         Returns:
             params
         """
-        return self._internal_net.init(params, rng_key)
+        params["internal_net"], state["internal_net"] = self._internal_net.init({}, {}, rng_key)
+        state["normalizer"] = {
+            "input": {
+                "center": jnp.zeros(shape=[self._preproc_obs_dim + self._action_dim]),
+                "scale": jnp.ones(shape=[self._preproc_obs_dim + self._action_dim]),
+            },
+            "output": {
+                "center": jnp.zeros(shape=[self._pred_dim]),
+                "scale": jnp.zeros(shape=[self._pred_dim])
+            }
+        }
+        return params, state
 
-    def fit_normalizer(self, obs, actions, next_obs) -> None:
+    def fit_normalizer(
+        self,
+        params: Dict,
+        state: Dict,
+        obs: Array,
+        actions: Array,
+        next_obs: Array
+    ) -> (Dict, Dict):
         """Fits the normalizer of this network to the given points.
 
         Args:
+            params: Dictionary of model parameters.
+            state: Dictionary of model state.
             obs: Array of observations.
             actions: Array of actions.
             next_obs: Array of next observations.
+
+        Returns:
+            Updated params and state reflecting fitted normalizer.
         """
         if obs.shape[0] != actions.shape[0] or actions.shape[0] != next_obs.shape[0]:
             raise RuntimeError("Arrays for fitting normalizer do not have matching lengths.")
@@ -113,7 +124,7 @@ class NeuralNetDynamicsModel:
             inputs = jnp.concatenate([jax.vmap(self._obs_preproc)(obs), actions], axis=-1)
             input_mean = jnp.mean(inputs, axis=0)
             input_stddev = jnp.std(inputs, axis=0)
-            self._normalizer_params["input"] = {
+            state["normalizer"]["input"] = {
                 "center": input_mean,
                 "scale": jnp.where(input_stddev > 1e-5, input_stddev, jnp.ones_like(input_stddev))
             }
@@ -122,14 +133,17 @@ class NeuralNetDynamicsModel:
             outputs = jax.vmap(self._targ_comp)(obs, next_obs)
             output_mean = jnp.mean(outputs, axis=0)
             output_stddev = jnp.std(outputs, axis=0)
-            self._normalizer_params["output"] = {
+            state["normalizer"]["output"] = {
                 "center": output_mean,
                 "scale": jnp.where(output_stddev > 1e-5, output_stddev, jnp.ones_like(output_stddev))
             }
 
+        return params, state
+
     def predict(
         self,
         params: Dict,
+        state: Dict,
         obs: Array,
         action: Array,
         rng_key=None
@@ -137,7 +151,8 @@ class NeuralNetDynamicsModel:
         """Returns the prediction of the model on a SINGLE observation and action.
 
         Args:
-            params: Dictionary of parameters.
+            params: Dictionary of model parameters.
+            state: Dictionary of model state.
             obs: Environment observation.
             action: Action.
             rng_key: JAX RNG key, only needed for probabilistic model. Do not reuse outside function.
@@ -145,7 +160,7 @@ class NeuralNetDynamicsModel:
         Returns:
             The predicted next observation according to the model.
         """
-        raw_output = self._compute_net_output(params, obs, action)
+        raw_output = self._compute_net_output(params, state, obs, action)
 
         if self._is_probabilistic:
             if rng_key is None:
@@ -155,13 +170,13 @@ class NeuralNetDynamicsModel:
         else:
             raw_prediction = raw_output
 
-        if self._is_normalizing_outputs:
-            raw_prediction = normalize(raw_prediction, self._normalizer_params["output"], invert=True)
+        raw_prediction = normalize(raw_prediction, state["normalizer"]["output"], invert=True)
         return self._next_obs_comp(obs, raw_prediction)
 
     def prediction_loss(
         self,
         params: Dict,
+        state: Dict,
         obs: Array,
         action: Array,
         next_obs: Array
@@ -173,7 +188,8 @@ class NeuralNetDynamicsModel:
         multivariate Gaussian with identity covariance.
 
         Args:
-            params: Dictionary of parameters.
+            params: Dictionary of model parameters.
+            state: Dictionary of model state.
             obs: Environment observation.
             action: Action.
             next_obs: Next environment observation.
@@ -181,10 +197,10 @@ class NeuralNetDynamicsModel:
         Returns:
             Negative log-likelihood.
         """
-        raw_output = self._compute_net_output(params, obs, action)
+        raw_output = self._compute_net_output(params, state, obs, action)
 
         if self._is_normalizing_outputs:
-            targ = normalize(self._targ_comp(obs, next_obs), self._normalizer_params["output"])
+            targ = normalize(self._targ_comp(obs, next_obs), state["normalizer"]["output"])
         else:
             targ = self._targ_comp(obs, next_obs)
 
@@ -195,12 +211,10 @@ class NeuralNetDynamicsModel:
 
         return -gaussian_log_prob(gaussian_params, targ)
 
-    def _compute_net_output(self, params, obs, action):
+    def _compute_net_output(self, params, state, obs, action):
         preproc_obs = self._obs_preproc(obs)
-        if self._is_normalizing_inputs:
-            return self._internal_net.forward(
-                params,
-                normalize(jnp.concatenate([preproc_obs, action]), self._normalizer_params["input"])
-            )
-        else:
-            return self._internal_net.forward(params, jnp.concatenate([preproc_obs, action]))
+        return self._internal_net.forward(
+            params["internal_net"],
+            state["internal_net"],
+            normalize(jnp.concatenate([preproc_obs, action]), state["normalizer"]["input"])
+        )

mbrl/policies/deterministic_policy.py
@@ -11,7 +11,6 @@ from mbrl._src.utils import Array
 class DeterministicPolicy:
     def __init__(
         self,
-        name: str,
         env: MujocoEnv,
         dummy_obs: Array,
         hidden_dims: List[int],
@@ -21,7 +20,6 @@ class DeterministicPolicy:
         """Creates a policy network.
 
         Args:
-            name: Model name. Used for referencing parameters, so must be unique.
             env: Environment. Used for inferring shapes and action bounds.
             dummy_obs: Observation from the environment, used for inferring shapes.
             hidden_dims: List of hidden dimensions for internal network.
@@ -30,7 +28,6 @@ class DeterministicPolicy:
             obs_preproc: Preprocesses observations before feeding into the network.
                 Defaults to identity operation.
         """
-        self._name = name
         self._action_bounds = (env.action_space.low, env.action_space.high)
         self._obs_preproc = obs_preproc
 
@@ -43,7 +40,6 @@ class DeterministicPolicy:
             return box_center + box_width * jnp.tanh(query)
 
         self._internal_net = FullyConnectedNeuralNet(
-            name="{}_internal".format(self._name),
             input_dim=preproc_obs_dim,
             output_dim=action_dim,
             hidden_dims=hidden_dims,
@@ -54,22 +50,25 @@ class DeterministicPolicy:
     def init(
         self,
         params: Dict,
+        state: Dict,
         rng_key: jax.random.KeyArray
     ) -> Dict:
         """Randomly initializes policy parameters, places them within the given parameter dictionary, and returns it.
 
         Args:
-            params: Dictionary of parameters where initialization will be placed.
+            params: Dictionary where initialization parameters will be placed.
+            state: Dictionary where policy network state will be placed.
             rng_key: JAX RNG key, which should not be reused outside this function.
 
         Returns:
             params
         """
-        return self._internal_net.init(params, rng_key)
+        return self._internal_net.init(params, state, rng_key)
 
     def act(
         self,
         params: Dict,
+        state: Dict,
         obs: Array,
         _rng_key=None
     ) -> jnp.ndarray:
@@ -83,4 +82,4 @@ class DeterministicPolicy:
         Returns:
             The action taken by the policy on the given observation.
         """
-        return self._internal_net.forward(params, self._obs_preproc(obs))
+        return self._internal_net.forward(params, state, self._obs_preproc(obs))

model_based_experiment.py
@@ -71,7 +71,7 @@ CONFIG = {
         "policy_training": {
             "plan_horizon": 20,
             "n_particles": 30,
-            "policy_optimizer": optax.adamw(1e-3),
+            "policy_optimizer": optax.adamw(1e-4),
             "n_policy_train_steps": 2000,
             "policy_train_batch_size": 32
         }
@@ -173,6 +173,7 @@ def main(
     logdir=None,
     save_every=1,
     keep_all_checkpoints=False,
+    n_init_trajs=1,
     n_iters=100,
     seed=0
 ):
@@ -194,7 +195,6 @@ def main(
         )
 
     dynamics_model = NeuralNetDynamicsModel(
-        name="dynamics_model",
         dummy_obs=env.reset(),
         dummy_action=env.action_space.sample(),
         **CONFIG[env_name]["dynamics"],
@@ -212,7 +212,6 @@ def main(
         )
     elif agent_type == "Policy":
         policy = DeterministicPolicy(
-            name="policy",
             env=env,
             dummy_obs=env.reset(),
             **CONFIG[env_name]["policy"],
@@ -230,7 +229,7 @@ def main(
     else:
         raise RuntimeError("Invalid agent type.")
 
-    for _ in trange(1, ncols=150, desc="Collecting initial trajectories"):
+    for _ in trange(n_init_trajs, ncols=150, desc="Collecting initial trajectories"):
         observations, actions, _ = rollout(env, CONFIG[env_name]["discount_factor"])
         agent.add_interaction_data(jnp.array(observations), jnp.array(actions))
 
@@ -289,6 +288,8 @@ if __name__ == "__main__":
                         help="How often agent checkpoints and videos will be saved.")
     parser.add_argument("--keep-all-checkpoints", action="store_true",
                         help="If provided, keeps all checkpoints (rather than only the most recent one).")
+    parser.add_argument("--n-init-trajs", type=int, default=1,
+                        help="Number of initial trajectories collected with random actions.")
     parser.add_argument("--iters", type=int, default=100,
                         help="Number of training iterations.")
     parser.add_argument("-s", type=int, default=-1,
@@ -305,6 +306,7 @@ if __name__ == "__main__":
         logdir=args.logdir,
         save_every=args.save_every,
         keep_all_checkpoints=args.keep_all_checkpoints,
+        n_init_trajs=args.n_init_trajs,
         n_iters=args.iters,
         seed=args.s if args.s != -1 else onp.random.randint(10000)
     )